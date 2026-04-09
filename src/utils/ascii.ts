57ffa02a85f474b3f98207dc9df12ba03e5d5e9b
Ryan Driscoll <driscollrp@gmail.com>
2026-04-09 12:46:26 -0600

    fix: get random char in inline script before paint

--- src/utils/ascii.ts
+++ src/utils/ascii.ts
@@ -1,8 +1,10 @@
 const width = 100
-const coolChars = ['»', '≡', '~', '┴', '¤', '/', '·']
-const defaultChar = '~'
+const defaultChar = ''
 
-export const filledRow = (char: string = defaultChar): string[] => Array(width).fill(char)
+export type AsciiChar = { char: string; grid: boolean }
+
+export const filledRow = (char: string = defaultChar): AsciiChar[] =>
+  Array(width).fill({ char, grid: true })
 
 export const createRow = ({
   text,
@@ -12,12 +14,24 @@ export const createRow = ({
   text: string
   char?: string
   right?: boolean
-}): string[] => {
-  const chars = Array(7).fill(char)
-  chars[right ? 'unshift' : 'push']('\u00A0', ...text.replace(/ /g, '\u00A0'), '\u00A0')
-  const padding = Array(Math.max(0, width - chars.length)).fill(char)
+}): AsciiChar[] => {
+  const g = (c: string): AsciiChar => ({ char: c, grid: true })
+  const t = (c: string): AsciiChar => ({ char: c, grid: false })
+
+  const chars: AsciiChar[] = Array(7).fill(g(char))
+  const textChars: AsciiChar[] = [
+    t('\u00A0'),
+    ...text.replace(/ /g, '\u00A0').split('').map(t),
+    t('\u00A0'),
+  ]
+
+  if (right) {
+    chars.unshift(...textChars)
+  } else {
+    chars.push(...textChars)
+  }
+
+  const padding: AsciiChar[] = Array(Math.max(0, width - chars.length)).fill(g(char))
   const strArray = right ? [...padding, ...chars] : [...chars, ...padding]
   return right ? strArray.reverse() : strArray
 }
-
-export const getCoolChar = () => coolChars[Math.floor(Math.random() * coolChars.length)]
