398a405d9ee057c1761f708c219bb784cf470738
Ryan Driscoll <driscollrp@gmail.com>
2026-04-10 15:11:20 -0600

    refactor: use class instead of data attribute for css pseudo elements

--- src/utils/ascii.ts
+++ src/utils/ascii.ts
@@ -1,10 +1,31 @@
 const width = 100
 const defaultChar = ''
 
-export type AsciiChar = { char: string; grid: boolean }
+export type AsciiChar = { char: string; className?: string }
 
 export const filledRow = (char: string = defaultChar): AsciiChar[] =>
-  Array(width).fill({ char, grid: true })
+  Array(width).fill({ char, className: 'grid' })
+
+const replaceSpace = (char: string): string => (char === ' ' ? '\u00A0' : char)
+
+export const createChar = ({
+  char,
+  className,
+}: {
+  char: string
+  className?: string
+}): AsciiChar => ({
+  char: replaceSpace(char),
+  className,
+})
+
+export const getCharsFromText = ({
+  text,
+  className,
+}: {
+  text: string
+  className?: string
+}) => text.split('').map((char) => createChar({ char, className }))
 
 export const createRow = ({
   text,
@@ -15,23 +36,23 @@ export const createRow = ({
   char?: string
   right?: boolean
 }): AsciiChar[] => {
-  const g = (c: string): AsciiChar => ({ char: c, grid: true })
-  const t = (c: string): AsciiChar => ({ char: c, grid: false })
-
-  const chars: AsciiChar[] = Array(5).fill(g(char))
+  const space = createChar({ char: ' ' })
+  let chars: AsciiChar[] = []
+  const minimumPadding = Array(5).fill(createChar({ char, className: 'grid' }))
   const textChars: AsciiChar[] = [
-    t('\u00A0'),
-    ...text.replace(/ /g, '\u00A0').split('').map(t),
-    t('\u00A0'),
+    space,
+    ...getCharsFromText({ text, className: 'text' }),
+    space,
   ]
+  const padding: AsciiChar[] = Array(Math.max(0, width - chars.length)).fill(
+    createChar({ char, className: 'grid' })
+  )
 
   if (right) {
-    chars.unshift(...textChars)
+    chars = [...padding, ...textChars, ...minimumPadding]
   } else {
-    chars.push(...textChars)
+    chars = [...minimumPadding, ...textChars, ...padding]
   }
 
-  const padding: AsciiChar[] = Array(Math.max(0, width - chars.length)).fill(g(char))
-  const strArray = right ? [...padding, ...chars] : [...chars, ...padding]
-  return right ? strArray.reverse() : strArray
+  return right ? chars.reverse() : chars
 }
